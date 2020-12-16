475d95b09f9bc3e75a5fd57d5b0b28956ef7b2e9
Kevin Hartmann <kevin.hartmann@gmail.com>
2020-12-16 15:45:30 -0500

    Basic Task One

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -2,7 +2,12 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
+
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.util.stream.Collectors;
 
 public class SampleClient {
 
@@ -21,7 +26,45 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
-
+        System.out.println(response.getEntry().stream().
+                map(Bundle.BundleEntryComponent::getResource).
+                filter(resource -> resource.hasType("Patient")).
+                map(resource -> (Patient) resource).
+                map(SampleClient::patientDetails).
+                sorted()
+                .collect(Collectors.joining("\n")));
     }
 
+    static String patientDetails(Patient patient) {
+        String first=firstName(patient);
+        String last=lastName(patient);
+        String birth = birthdate(patient);
+        return first + " " + last + " " + birth;
+    }
+    static String firstName(Patient patient) {
+        // TODO: Confirm handling of people with multiple names, no given name, no family name, etc.
+        if (!patient.hasName()) {
+            return "Nameless";
+        }
+        return patient.getName().stream().
+                filter(name -> name.hasGiven()).
+                map(n -> n.getGiven().get(0).toString()).
+                findFirst().orElse("NoFirstNameAvailable");
+    }
+    static String lastName(Patient patient) {
+        if (!patient.hasName()) {
+            return "Nobody";
+        }
+
+        return patient.getName().stream().
+                filter(HumanName::hasFamily).
+                map(HumanName::getFamily).findFirst().orElse("NoLastNameAvailable");
+    }
+
+    static String birthdate(Patient patient) {
+        if (!patient.hasBirthDate()) {
+            return "Unknown Date of Birth";
+        }
+        return patient.getBirthDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString();
+    }
 }
